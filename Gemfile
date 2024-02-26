81e13fc5057572bf78a0a02bc494257512785664
vitalii.mozol <vitalii.mozol@teladochealth.com>
2024-02-26 22:57:55 +0200

    added user model and devise gem auth

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 gem 'bootstrap', '~> 5.2.0'
+gem 'devise'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nickname, :phone_number, :full_name, :email, :password) }
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:nickname, :phone_number, :full_name, :email, :password, :current_password)
+    end
+  end
 end
